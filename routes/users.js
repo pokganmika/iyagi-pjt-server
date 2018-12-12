6af9c80c256cbcde0415402f6de031b3186ce002
calmgust <calmgust@gmail.com>
2018-12-12 11:39:59 +0900

    [+]index update

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const User = require('../models/user');
 const router = express.Router();
 
 
-router.post('/users', function(req, res, next) {
-  
+router.post('/users', async (req, res, next) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    thumbnail: req.body.thumnail,
+  });
+  await user.save()
+
 });
+
 
 module.exports = router;
