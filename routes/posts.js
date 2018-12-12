6ed6cb909e85091411c5e6c4cf161da89257ef09
김다해 <dhkim1014@gmail.com>
2018-12-12 09:41:13 +0900

    working on implementing routes

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const { Post } = require('../models/post');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/posts', function(req, res, next) {
-  res.send();
+router.get('/', async (req, res) => {
+  const posts = await Post
+    .find()
+    .populate('writer') // ???
+    .sort([['_id', 1]]);
+  res.send(posts);
 });
+
+router.post('/', async (req, res) => { // add authentication middleware 
+  // const { error } = validate(req.body); 
+  // if (error) return res.status(400).send(error.details[0].message);
+
+  let post = new Post({ 
+    content: req.body.content,
+    writer: req.body.id // ???
+  });
+  post = await post.save();
+  
+  res.send(post);
+})
+
 
 module.exports = router;
