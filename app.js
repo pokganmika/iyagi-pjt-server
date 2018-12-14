d4844bb061e4cc5924705bf62287f0ac6315d7f7
dahaekim88 <dhkim1014@gmail.com>
2018-12-14 20:09:24 +0900

    set up connection to mysql

--- app.js
+++ app.js
@@ -14,11 +14,12 @@ const storiesRouter = require('./routes/stories');
 const usersRouter = require('./routes/users');
 const sequelize = require('./models').sequelize;
 // const { sequelize } = require('./models');
-const passportConfig = require('./passport');
+// const passportConfig = require('./passport');
+const db = require('./models'); // mysql connect
 
 const app = express();
-sequelize.sync();
-passportConfig(passport);
+// sequelize.sync();
+// passportConfig(passport);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -39,9 +40,9 @@ app.use(session({
   },
 }));
 
-app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(flash());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/posts', postsRouter);
@@ -69,3 +70,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
--- models/index.js
+++ models/index.js
@@ -13,7 +13,7 @@ const config = require(__dirname + '/../config/config.json')[env];
 
 // module.exports = db;
 
-let conn = mysql.createConnection({
+var conn = mysql.createConnection({
   host: 'localhost',
   user: config.username,
   password: config.password,
@@ -25,76 +25,6 @@ conn.connect((err) => {
     return console.error('error: ' + err.message);
   }
   console.log('Connected to MySQL server');
-
-  let createUsers = `
-  CREATE TABLE IF NOT EXISTS users(
-    userId VARCHAR(30) PRIMARY KEY,
-    password VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    thumbnail VARCHAR(255),
-    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-  )`;
-
-  conn.query(createUsers, (err, results, fields) => {
-    if (err) {
-      console.log(err.message);
-    }
-    console.log('created users table');
-  });
-
-  let createStories = `
-  CREATE TABLE IF NOT EXISTS stories(
-    storyId INT PRIMARY KEY AUTO_INCREMENT,
-    views INT DEFAULT 0,
-    isDone TINYINT(1) NOT NULL DEFAULT 0
-  )`;
-
-  conn.query(createStories, (err, results, fields) => {
-    if (err) {
-      console.log(err.message);
-    }
-    console.log('created stories table');
-  });
-
-  let createPosts = `
-  CREATE TABLE IF NOT EXISTS posts(
-    postId INT PRIMARY KEY AUTO_INCREMENT,
-    userId VARCHAR(30) NOT NULL,
-    content TEXT NOT NULL,
-    postedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    thumbsUp INT DEFAULT 0,
-    thumbsDown INT DEFAULT 0,
-    storyId INT NOT NULL,
-    FOREIGN KEY (userId) REFERENCES users(userId),
-    FOREIGN KEY (storyId) REFERENCES stories(storyId)
-  )`;
-
-  conn.query(createPosts, (err, results, fields) => {
-    if (err) {
-      console.log(err.message);
-    }
-    console.log('created posts table');
-
-  });
-
-  let createComments = `
-  CREATE TABLE IF NOT EXISTS comments(
-    commentId INT PRIMARY KEY AUTO_INCREMENT,
-    userId VARCHAR(30) NOT NULL,
-    content TEXT NOT NULL,
-    storyId INT NOT NULL,
-    FOREIGN KEY (userId) REFERENCES users(userId),
-    FOREIGN KEY (storyId) REFERENCES stories(storyId)
-  )`;
-
-  conn.query(createComments, (err, results, fields) => {
-    if (err) {
-      console.log(err.message);
-    }
-    console.log('created comments table');
-
-  });
-
 });
 
 module.exports = {
