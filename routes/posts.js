117178e9107870a21919d6355e9cddacd115d480
dahaekim88 <dhkim1014@gmail.com>
2018-12-14 20:10:50 +0900

    implement CRUD operations

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,76 @@
+var db = require('../models');
+var mysql = require('mysql2/promise');
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.send();
+// 연재 게시물
+router.get('/', async (req, res) => {
+  var sql = `
+    SELECT u.userId, u.thumbnail, p.postId, p.content, p.postedAt, p.thumbsUp, p.thumbsDown
+    FROM users u INNER JOIN posts p
+    ON u.userId = p.userId
+    WHERE p.storyId = (SELECT storyId FROM stories WHERE isDone = false);`;
+
+  await db.conn.execute(sql, (err, results) => {
+    if (err) console.err;
+    res.send(results);
+  });
+});
+
+// 연재글 작성
+router.post('/', async (req, res) => { // add authentication middleware
+  var userId = req.body.writer; // 로그인한 사용자의 아이디
+  var content = req.body.content;
+  console.log('userId: ' + userId + ', content: ' + content);
+
+  var sql = `SELECT storyId FROM stories WHERE isDone = false`;
+  
+  var ongoingStoryId = 0;
+  await db.conn.execute(sql, async (err, result) => {
+    if (err) console.err;
+    console.log(result.length);
+    if(result.length === 0) {
+      var insertStory = `INSERT INTO stories(isDone) VALUES(false);`;
+      await db.conn.execute(insertStory, async (err) => {
+        if (err) console.err;
+        ongoingStoryId = await db.conn.execute(sql);
+        console.log('new story saved', ongoingStoryId);
+      })
+    } else {
+      ongoingStoryId = result[0].storyId;
+      console.log(ongoingStoryId);
+    }
+
+    var insertPost = `
+      INSERT INTO posts(userId, content, storyId) 
+      VALUES('${userId}', '${content}', ${ongoingStoryId})`;
+
+    await db.conn.execute(insertPost, (err) => {
+      if (err) console.err;
+      // foreign key가 맞지 않는 경우 저장 X -> 에러 처리
+      res.send('new post saved!');
+    });
+  });
+});
+
+// 미리보기 화면에서 완결 버튼 클릭
+router.patch('/', async (req, res) => {
+  var sql = `SELECT storyId FROM stories WHERE isDone = false`;
+
+  var ongoingStoryId = 0;
+  await db.conn.execute(sql, async (err, result) => {
+    if (err) console.err;
+    ongoingStoryId = result[0].storyId;
+    console.log(ongoingStoryId);
+
+    var updateStory = `UPDATE stories SET isDone = true WHERE storyId = ${ongoingStoryId}`;
+
+    await db.conn.execute(updateStory, (err) => {
+      if (err) console.err;
+      res.send('story closed!');
+    })
+  });
+
 });
 
 module.exports = router;
--- routes/stories.js
+++ routes/stories.js
@@ -1,8 +1,80 @@
+var db = require('../models');
+var mysql = require('mysql2/promise');
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.send();
+// 완결 게시물 리스트
+router.get('/', async (req, res) => {
+  var sql = `
+    SELECT s.storyId, s.views, p.postId, p.userId, p.content, p.thumbsUp, p.thumbsDown
+    FROM stories s INNER JOIN posts p
+    ON s.storyId = p.storyId
+    WHERE s.isDone = true
+    ORDER BY s.storyId, p.postId
+  `; // ***** comment, user table join *****
+  
+  await db.conn.execute(sql, (err, results) => {
+    if (err) console.err;
+    res.send(results);
+  });
+});
+
+// 단일 게시물 클릭
+router.get('/:id', async (req, res) => {
+  var clickedStory = req.params.id;
+
+  var updateViews = `UPDATE stories SET views=views+1 WHERE storyId = ${clickedStory};`;
+
+  await db.conn.execute(updateViews, async (err) => {
+    if (err) console.err;
+
+    var sql = `
+      SELECT s.storyId, s.views, p.postId, p.userId, p.content, p.thumbsUp, p.thumbsDown
+      FROM stories s INNER JOIN posts p 
+      ON s.storyId = p.storyId
+      WHERE s.storyId = ${clickedStory}
+    `; // ***** comment, user table join *****
+
+    await db.conn.execute(sql, async (err, result) => {
+      res.send(result);
+    });
+  });
+});
+
+// 댓글 작성
+router.post('/:id', async (req, res) => {
+  var clickedStory = req.params.id;
+  var userId = req.body.writer;
+  var content = req.body.content;
+
+  var insertComment = `
+    INSERT INTO comments(userId, content, storyId) 
+    VALUES('${userId}', '${content}', ${clickedStory})
+  `;
+  
+  await db.conn.execute(insertComment, (err) => {
+    if (err) console.err;
+    res.send('new comment saved!');
+  });
+});
+
+// 댓글 수정
+router.patch('/:id', async (req, res) => {
+  var updatedComment = req.body.id; // commentId
+  var userId = req.body.writer;
+  var updatedContent = req.body.content;
+
+  console.log(`${updatedComment}, ${updatedContent}`);
+  
+  var updateComment = `
+    UPDATE comments SET content = '${updatedContent}'
+    WHERE commentId = ${updatedComment}
+  `;
+  
+  await db.conn.execute(updateComment, (err, results) => {
+    if (err) console.err;
+    res.send('comments updated!');
+  });
 });
 
 module.exports = router;
