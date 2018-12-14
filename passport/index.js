a4b1d5b3a223a06b56211d98fabe74c3233ec433
calmgust <calmgust@gmail.com>
2018-12-14 21:13:43 +0900

    [+]passport setup

---
+++ passport/index.js
@@ -0,0 +1,19 @@
+const local = require('./localStrategy');
+const kakao = require('./kakaoStrategy');
+const { User } = require('../models'); // 정리 필요
+
+module.exports = passport => {
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.find({ where : { id } })
+      .then(user => done(null, user))
+      .catch( err => done(err));
+  });
+
+  local(passport);
+  kakao(passport);
+};
+
